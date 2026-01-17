c275f994a8d5be004b622ac3ae6e69fef0a0c4c1
Leo Mundin <leomundin@hotmail.co.uk>
2026-01-17 15:41:47 +0000

    Squashed commit of the following:
    commit 83fd52e42457c87b5f95a261876822ba9f8ff276
    Author: Leo Mundin <leomundin@hotmail.co.uk>
    Date:   Wed Dec 31 15:34:10 2025 +0000
    
        DEV-Audio : SoLoud Libary
    
        Implemented the SoLoud Libary for easily playing audio. System needs updating for proper implementation into the game engine

---
+++ Shader-Engine_OpenGL/Source/Engine/Audio/SoLoud/soloud_codegen.cpp
@@ -0,0 +1,12 @@
+// soloud_codegen.cpp
+#define STB_VORBIS_IMPLEMENTATION
+#include "stb_vorbis.h"
+
+#define DR_WAV_IMPLEMENTATION
+#include "dr_wav.h"
+
+#define DR_MP3_IMPLEMENTATION
+#include "dr_mp3.h"
+
+#define DR_FLAC_IMPLEMENTATION
+#include "dr_flac.h"
--- Shader-Engine_OpenGL/Source/Main.cpp
+++ Shader-Engine_OpenGL/Source/Main.cpp
@@ -1,5 +1,9 @@
 #include "Game/ElderHex.h"
 
+#include "soloud.h"
+#include "soloud_wav.h"
+#include <iostream>
+
 
 const int SCREEN_WIDTH = 1600;
 const int SCREEN_HEIGHT = 1200;
@@ -7,6 +11,23 @@ const int SCREEN_HEIGHT = 1200;
 ElderHex game(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 int main(){
+
+    // TO-DO : Move to seperate system with sound objects for audio handling.
+    // Start Music
+    SoLoud::Soloud audio;
+    audio.init();        
+
+    SoLoud::Wav music; 
+    if (music.load("Assets/Audio/Skyrim_16-bit_Theme.mp3") != 0)
+    {
+        std::cout << "Failed to load audio!\n";
+        return 1;
+    } 
+
+    music.setVolume(0.5);
+    audio.play(music);
+
+    // Start Game
     game.init();
     return 0;
 
